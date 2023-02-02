8cb8c83b100928a87573c1c65dc54448e91a59e4
root <joe@morris.cloud>
2023-02-02 04:51:33 +0000

    splitting setup between first-time (downloads) and subsequent-time (package installs only) for machines like on paperspace that get destroyed but keep filesystem

---
+++ setup-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#The intent here is to set up a server that can do the things the scripts require. I'm using runpod.io servers that already have pytorch installed; they use /workspace as a root working directory. Change BASEDIR as appropriate or your setup. 
+
+apt update
+apt --yes install libsndfile1-dev ffmpeg emacs python3.9-dev nq less
+
+export BASEDIR=/notebooks
+export TORTOISE_MODELS_DIR=$BASEDIR/models
+export NQDIR=$BASEDIR/outputs
+cd $BASEDIR
+mkdir inputs
+mkdir logs
+mkdir outputs
+mkdir models
+git clone https://github.com/neonbjb/tortoise-tts
+git clone https://github.com/NVIDIA/NeMo-text-processing/
+cp -r  wikipedia-tts/voices/* tortoise-tts/tortoise/voices/
+python -m pip install -r tortoise-tts/requirements.txt
+python -m pip install wikipedia Cython pynini
+python -m pip install git+https://github.com/NVIDIA/NeMo.git@main#egg=nemo_toolkit[nlp]
+
+cd tortoise-tts
+
+python -m pip install .
+
+cd models
+wget `../models.txt`
---
+++ setup-machine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#The intent here is to set up a server that can do the things the scripts require. I'm using runpod.io servers that already have pytorch installed; they use /workspace as a root working directory. Change BASEDIR as appropriate or your setup. 
+
+apt update
+apt --yes install libsndfile1-dev ffmpeg emacs python3.9-dev nq less task-spooler
+
+export BASEDIR=/notebooks
+export TORTOISE_MODELS_DIR=$BASEDIR/models
+export NQDIR=$BASEDIR/outputs
+cd $BASEDIR
+
+python -m pip install -r tortoise-tts/requirements.txt
+python -m pip install wikipedia Cython pynini
+python -m pip install git+https://github.com/NVIDIA/NeMo.git@main#egg=nemo_toolkit[nlp]
+
+cd tortoise-tts
+
+python -m pip install .
