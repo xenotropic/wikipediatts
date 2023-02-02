f7296c0598bd96ea52a5fef87b68a12bf686d900
root <joe@morris.cloud>
2023-02-02 17:24:19 +0000

    making read wiki actually just read

--- readwiki.sh
+++ readwiki.sh
@@ -2,6 +2,4 @@
 cd $BASEDIR
 WIKIARTICLE=$1
 mkdir $BASEDIR/inputs/$WIKIARTICLE
-python wikipedia-tts/wikiparse.py $WIKIARTICLE > $BASEDIR/inputs/$WIKIARTICLE.txt
 python wikipedia-tts/wikiparse.py $WIKIARTICLE > $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt
-nq python $BASEDIR/tortoise-tts/tortoise/read.py --model_dir $BASEDIR/models --textfile $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt --voice train_e --preset=fast --output_path=$BASEDIR/outputs > /$BASEDIR/logs/$WIKIARTICLE.log 2>&1 
