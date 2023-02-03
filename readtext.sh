30802719a04658ab0f40f1a23a24c67e678edaea
root <joe@morris.cloud>
2023-02-03 06:08:42 +0000

    add lines to try to avoid re-downloading models add mp3 conversion

--- readtext.sh
+++ readtext.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd $BASEDIR
 WIKIARTICLE=$1
+VOICE=train_winston
+TORTOISE_MODELS_DIR=$BASEDIR/models
 mkdir $BASEDIR/outputs/$WIKIARTICLE
 python $BASEDIR/tortoise-tts/tortoise/read.py --model_dir $BASEDIR/models --textfile $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt --voice train_winston --preset=fast --output_path=$BASEDIR/outputs/$WIKIARTICLE > /$BASEDIR/logs/$WIKIARTICLE.log 2>&1 &
+ffmpeg -i $BASEDIR/outputs/$WIKIARTICLE/$VOICE/combined.wav $BASEDIR/outputs/$WIKIARTICLE/$VOICE/$WIKIARTICLE.mp3 -metadata title="Wikipedia article on $WIKIARTICLE read by a text to speech voice, see https://github.com/xenotropic/wikipedia-tts/"
