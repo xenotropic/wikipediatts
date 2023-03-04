a73868218d58f7731515c25528e6d7ddb3ff6c78
Joseph Morris <joe@morris.cloud>
2023-03-03 17:48:08 -0800

    minor changes

--- readtext.sh
+++ readtext.sh
@@ -4,5 +4,5 @@ WIKIARTICLE=$1
 VOICE=train_winston
 TORTOISE_MODELS_DIR=$BASEDIR/models
 mkdir $BASEDIR/outputs/$WIKIARTICLE
-python $BASEDIR/tortoise-tts/tortoise/read.py --model_dir $BASEDIR/models --textfile $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt --voice train_winston --preset=fast --output_path=$BASEDIR/outputs/$WIKIARTICLE > /$BASEDIR/logs/$WIKIARTICLE.log 2>&1 &
+python3 $BASEDIR/tortoise-tts/tortoise/read.py --model_dir $BASEDIR/models --textfile $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt --voice train_e --preset=fast --output_path=$BASEDIR/outputs/$WIKIARTICLE > /$BASEDIR/logs/$WIKIARTICLE.log 2>&1 &
 ffmpeg -i $BASEDIR/outputs/$WIKIARTICLE/$VOICE/combined.wav $BASEDIR/outputs/$WIKIARTICLE/$VOICE/$WIKIARTICLE.mp3 -metadata title="Wikipedia article on $WIKIARTICLE read by a text to speech voice, see https://github.com/xenotropic/wikipedia-tts/"
