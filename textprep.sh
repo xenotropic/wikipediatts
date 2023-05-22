23e64e21eec46c8e1bce66336b1530f66014b2a5
Joseph Morris <joe@morris.cloud>
2023-05-21 17:51:07 -0700

    sending text prep errors to separate file

--- textprep.sh
+++ textprep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $BASEDIR
 WIKIARTICLE=$1
-mkdir $BASEDIR/inputs/$WIKIARTICLE
-python3.9 wikipedia-tts/wikiparse.py $WIKIARTICLE > $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt
+mkdir -p $BASEDIR/inputs/$WIKIARTICLE
+python3.9 wikipedia-tts/wikiparse.py $WIKIARTICLE > $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.txt 2>  $BASEDIR/inputs/$WIKIARTICLE/$WIKIARTICLE.errors
