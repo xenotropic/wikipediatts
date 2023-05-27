9d538335689b27046ef029248af071445270ff61
Joseph Morris <joe@morris.cloud>
2023-05-27 12:08:34 -0700

    removing periods since that is now handled in preprocessor, chunking not just by sentences

--- wikiparse.py
+++ wikiparse.py
@@ -17,7 +17,6 @@ tokenstring="This is the Wikipedia article on " + title + " read by a text to sp
     #TODO: need to further tokenize these based on parens (long parens only?), dashes (n and m), semicolons, quotes
 for sen in sentences:
     tokenstring+=sen + "|\n"
-#    tokenstring+=sen + ". "
     if (len(sen) > 360): print ("******ERROR: string over 360 characters: " + sen + "\n", file=sys.stderr )
 
 tokenstring+="This concludes the Wikipedia article on " + title + "| This recording and the underlying text are licensed under the Creative Commons Attribution Share Alike 3 point 0 Unported License|except for copyrighted excerpts and quotations which are used on the basis of fair use." 
